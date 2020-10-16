fda2412601dca5399737dfb9c5dab38f5665165a
ultrabear <bearodark@gmail.com>
2020-10-16 15:53:53 -0700

    change style from PEP8 to YAPF+tabs

--- SKCY11X.py
+++ SKCY11X.py
@@ -1,31 +1,37 @@
 import random, lzma, hashlib
 
-def pack(byte): 
+
+def pack(byte):
 	return int(byte, 2)
+
+
 def unpack(bits):
 	dat = str(bin(bits)).split("b")[1]
-	dat = ((8-len(dat))*"0")+dat
+	dat = ((8 - len(dat)) * "0") + dat
 	return dat
- 
-def fromfile(byte): 
+
+
+def fromfile(byte):
 	bytelen = len(byte)
 	bits = ""
 	for index, i in enumerate(byte):
 		dat = str(bin(i)).split("b")[1]
-		dat = ((8-len(dat))*"0")+dat
-		bits+=dat
-	byte = bits			 
+		dat = ((8 - len(dat)) * "0") + dat
+		bits += dat
+	byte = bits
 	return byte
 
-def tofile(outputData): 
-	outputData = [outputData[i*8:i*8+8] for i in range(int(len(outputData)/8))]
+
+def tofile(outputData):
+	outputData = [outputData[i * 8:i * 8 + 8] for i in range(int(len(outputData) / 8))]
 	byte = []
 	for index, i in enumerate(outputData):
 		byte.append(int(i, 2))
 	outputData = bytes(byte)
 	return outputData
 
-def runskip(indata,key,gatepos): 
+
+def runskip(indata, key, gatepos):
 	data = indata
 	if gatepos:
 		#gate true
@@ -33,134 +39,142 @@ def runskip(indata,key,gatepos):
 	else:
 		gate = 1
 	le = len(data)
-	for i in range(gate,len(data),2):
-		if not(i+key >= le):
-			data[i+key], data[i] = data[i], data[i+key]
+	for i in range(gate, len(data), 2):
+		if not (i + key >= le):
+			data[i + key], data[i] = data[i], data[i + key]
 	return data
 
-def unskip(indata, key, gatepos): 
+
+def unskip(indata, key, gatepos):
 	data = indata
 	if gatepos:
 		# gate true
-		if ((len(indata)-key)%2 == 1):
+		if ((len(indata) - key) % 2 == 1):
 			gate = 0
 		else:
-			gate = 1		
+			gate = 1
 	else:
-		if ((len(indata)-key)%2 == 0):
+		if ((len(indata) - key) % 2 == 0):
 			gate = 0
 		else:
 			gate = 1
 	le = len(data)
 	data = data[::-1]
-	for i in range(gate,len(data),2):
-		if not(i+key >= le):
-			data[(i+key)], data[i] = data[i], data[(i+key)]				 
+	for i in range(gate, len(data), 2):
+		if not (i + key >= le):
+			data[(i + key)], data[i] = data[i], data[(i + key)]
 	data = data[::-1]
 	return data
 
-def offsetter(data): 
+
+def offsetter(data):
 	return (eprintdct[data])
-	
-def unoffsetter(data): 
+
+
+def unoffsetter(data):
 	return (dprintdct[data])
 
-def runoffset(data,key1,key2,key3):
+
+def runoffset(data, key1, key2, key3):
 	global eprintdct
-	data = [data[i:i+8] for i in range(0,len(data),8)]
-	printls = [] 
+	data = [data[i:i + 8] for i in range(0, len(data), 8)]
+	printls = []
 	for i in range(256):
 		loop = str(bin(i)).split("b")[1]
-		printls.append(((8-len(loop))*"0")+loop)
+		printls.append(((8 - len(loop)) * "0") + loop)
 	printls = runskip(printls, key1, True)
 	printls = runskip(printls, key2, False)
 	eprintdct = {}
 	for i in printls:
 		tmpkey = key3
-		while printls.index(i)+tmpkey >= len(printls):
+		while printls.index(i) + tmpkey >= len(printls):
 			tmpkey -= len(printls)
-		eprintdct[i] = printls[printls.index(i)+tmpkey]
+		eprintdct[i] = printls[printls.index(i) + tmpkey]
 	out = []
 	for index, i in enumerate(data):
 		out.append(offsetter(i))
 	data = "".join(out)
 	return data
 
-def rundeoffset(data,key1,key2,key3):
+
+def rundeoffset(data, key1, key2, key3):
 	global dprintdct
-	printls = [] 
+	printls = []
 	for i in range(256):
 		loop = str(bin(i)).split("b")[1]
-		printls.append(((8-len(loop))*"0")+loop)
+		printls.append(((8 - len(loop)) * "0") + loop)
 	printls = runskip(printls, key1, True)
 	printls = runskip(printls, key2, False)
-	data = [data[i:i+8] for i in range(0,len(data),8)]
+	data = [data[i:i + 8] for i in range(0, len(data), 8)]
 	dprintdct = {}
 	for i in printls:
 		tmpkey = key3
-		while printls.index(i)-tmpkey < 0:
+		while printls.index(i) - tmpkey < 0:
 			tmpkey -= len(printls)
-		dprintdct[i] = printls[printls.index(i)-tmpkey] 
+		dprintdct[i] = printls[printls.index(i) - tmpkey]
 	out = []
 	for index, i in enumerate(data):
 		out.append(unoffsetter(i))
 	data = "".join(out)
 	return data
 
-def skippingencode(data, lkey): 
-	lkey = [int("0x"+lkey[i:i+2],0) for i in range(0,len(lkey),2)]
-	data = runoffset(data,lkey[4],lkey[2],lkey[3]) 
+
+def skippingencode(data, lkey):
+	lkey = [int("0x" + lkey[i:i + 2], 0) for i in range(0, len(lkey), 2)]
+	data = runoffset(data, lkey[4], lkey[2], lkey[3])
 	data = [i for i in data]
-	data = (runskip(data, lkey[0]+lkey[6], True))
+	data = (runskip(data, lkey[0] + lkey[6], True))
 	data = (runskip(data, lkey[1], False))
-	for i in range(lkey[5]*16):
-		data.append(data[random.randint(0,len(data)-1)])
+	for i in range(lkey[5] * 16):
+		data.append(data[random.randint(0, len(data) - 1)])
 	data = "".join(data)
-	data = runoffset(data,lkey[7],lkey[8],lkey[9])
+	data = runoffset(data, lkey[7], lkey[8], lkey[9])
 	return (data)
 
-def skippingdecode(data, lkey): 
-	lkey = [int("0x"+lkey[i:i+2],0) for i in range(0,len(lkey),2)]
-	data = rundeoffset(data,lkey[7],lkey[8],lkey[9])
+
+def skippingdecode(data, lkey):
+	lkey = [int("0x" + lkey[i:i + 2], 0) for i in range(0, len(lkey), 2)]
+	data = rundeoffset(data, lkey[7], lkey[8], lkey[9])
 	data = [i for i in data]
-	for i in range(lkey[5]*16):
+	for i in range(lkey[5] * 16):
 		try:
 			del data[-1]
-		except IndexError: # error in small files with wrong passwords
+		except IndexError:  # error in small files with wrong passwords
 			raise ValueError("The decrypted data does not match the SHA256 hash")
 	data = (unskip(data, lkey[1], False))
-	data = (unskip(data, lkey[0]+lkey[6], True))
+	data = (unskip(data, lkey[0] + lkey[6], True))
 	data = "".join(data)
-	data = rundeoffset(data,lkey[4],lkey[2],lkey[3])
+	data = rundeoffset(data, lkey[4], lkey[2], lkey[3])
 	return (data)
 
+
 def runencode(inputfilename, outputfile, key):
-	with open(inputfilename,"rb") as txt:
+	with open(inputfilename, "rb") as txt:
 		inputfile = fromfile(txt.read())
 	with open(outputfile, "wb+") as txt:
-		txt.write(tofile(skippingencode(inputfile,key)))
+		txt.write(tofile(skippingencode(inputfile, key)))
+
 
 def rundecode(inputfilename, outputfile, key):
-	with open(inputfilename,"rb") as txt:
+	with open(inputfilename, "rb") as txt:
 		inputfile = fromfile(txt.read())
 	with open(outputfile, "wb+") as txt:
-		txt.write(tofile(skippingdecode(inputfile,key)))
+		txt.write(tofile(skippingdecode(inputfile, key)))
 
 
-class fileio: # object format fileio wrapper for SKCY11X, includes LZMA compression and sha256 checksumming
-
-	def __enter__(self, filename, password): # with operator init
+class fileio:  # object format fileio wrapper for SKCY11X, includes LZMA compression and sha256 checksumming
+	def __enter__(self, filename, password):  # with operator init
 		self.filename = filename
 		prehash = hashlib.sha256(password.encode("utf8")).digest()
 		self.password = hex(int.from_bytes(prehash, "big"))[2:22]
 
-	def __init__(self, filename, password): # init data
+	def __init__(self, filename, password):  # init data
 		self.filename = filename
 		prehash = hashlib.sha256(password.encode("utf8")).digest()
 		self.password = hex(int.from_bytes(prehash, "big"))[2:22]
 
-	def write(self, towrite: bytes): # writes to file
-		writeto = open(self.filename, "wb") 
+	def write(self, towrite: bytes):  # writes to file
+		writeto = open(self.filename, "wb")
 		compressed = lzma.compress(towrite)
 		shahash = hashlib.sha256(compressed).digest()
 		encrypted = tofile(skippingencode(fromfile(compressed), self.password))
@@ -169,11 +183,11 @@ class fileio: # object format fileio wrapper for SKCY11X, includes LZMA compress
 		writeto.write(encrypted)
 		writeto.close()
 
-	def read(self): # reads from file
+	def read(self):  # reads from file
 		readfrom = open(self.filename, "rb")
 		if readfrom.read(10) == b"SKCY11Xof\x00":
 			testhash = readfrom.read(32)
-			data = tofile(skippingdecode(fromfile(readfrom.read()),self.password))
+			data = tofile(skippingdecode(fromfile(readfrom.read()), self.password))
 			readfrom.close()
 			if hashlib.sha256(data).digest() == testhash:
 				return lzma.decompress(data)
@@ -182,12 +196,11 @@ class fileio: # object format fileio wrapper for SKCY11X, includes LZMA compress
 		else:
 			readfrom.close()
 			raise TypeError("The requested file is not a supported SKCY11X object format version, or in SKCY11X object format")
-	
-	def __exit__(self): # with operator deletes obj params
+
+	def __exit__(self):  # with operator deletes obj params
 		del self.password
 		del self.filename
 
-	def close(self): # deletes obj params
+	def close(self):  # deletes obj params
 		del self.password
 		del self.filename
-
--- main.py
+++ main.py
@@ -3,13 +3,12 @@
 import getpass
 import discord
 
-
 import logger
 from discord.ext import commands
 
 from SKCY11X import fileio as SKCYfileio
 
-print("BIGMANAGE BOT X20201014") # X means borked, B is beta, R is release
+print("BIGMANAGE BOT X20201014")  # X means borked, B is beta, R is release
 
 print("INIT_TOKEN")
 TOKENFILE = SKCYfileio(".bot_token", getpass.getpass())
@@ -19,30 +18,26 @@ TOKENFILE.close()
 logger.start()
 
 # initialize discord bot
-bot = commands.Bot(command_prefix='%', description='',
-                   case_insensitive=True)
+bot = commands.Bot(command_prefix='%', description='', case_insensitive=True)
 
 bot.run(TOKEN)
 
 
 @bot.command(name="test", help="Responds with 'works!'")
 async def test(ctx: commands.Context):
-    print('test triggered!')
-    await ctx.send("works!")
+	print('test triggered!')
+	await ctx.send("works!")
 
 
 @bot.event
 async def on_ready():
-    print(
-        f'{bot.user} is connected to Discord\n'
-        f'With {bot.command_prefix} as prefix\n'
-        f"Can be mentioned with <@!{bot.user.id}>"
-    )
+	print(f'{bot.user} is connected to Discord\n' f'With {bot.command_prefix} as prefix\n' f"Can be mentioned with <@!{bot.user.id}>")
 
 
 @bot.event
 async def on_connect():
-    print("Connected!")
+	print("Connected!")
+
 
 '''
 @bot.event
@@ -57,27 +52,26 @@ async def on_member_join(member: discord.Member):
 
 @bot.event
 async def on_message(message: discord.Message):
-    if message.author == bot.user:
-        return 0
-    try:
-        await bot.process_commands(message)
-    except AttributeError:
-        print("Could not process commands \n"
-              "This is could be due to bot not being started \n"
-              "This is normal for testing")
+	if message.author == bot.user:
+		return 0
+	try:
+		await bot.process_commands(message)
+	except AttributeError:
+		print("Could not process commands \n" "This is could be due to bot not being started \n" "This is normal for testing")
 
 
 @bot.event
 async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
+	with open('err.log', 'a') as f:
+		if event == 'on_message':
+			f.write(f'Unhandled message: {args[0]}\n')
 
 
 @bot.event
 async def on_command_error(ctx, error):
-    await ctx.send(error)
-    print(error)
+	await ctx.send(error)
+	print(error)
+
 
 '''
 def exit_handler():
