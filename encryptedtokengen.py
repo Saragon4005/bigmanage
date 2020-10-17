8e836947afae2db8546e5addff09987f017109f4
ultrabear <bearodark@gmail.com>
2020-10-16 18:37:07 -0700

    add YAPF

--- encryptedtokengen.py
+++ encryptedtokengen.py
@@ -1,11 +1,13 @@
 from SKCY11X import fileio
 import getpass
 
+
 def gentoken():
 	token = input("Paste discord token: ")
 	fileobj = fileio(".bot_token", getpass.getpass("Enter password: "))
 	fileobj.write(token.encode("utf8"))
 	fileobj.close()
+
 
 if __name__ == "__main__":
 	gentoken()
