218eb7396992a87514073d9ac6ddb67b343a8c53
ultrabear <bearodark@gmail.com>
2020-10-16 18:35:18 -0700

    make encryptedtokengen more modular

--- encryptedtokengen.py
+++ encryptedtokengen.py
@@ -1,7 +1,11 @@
 from SKCY11X import fileio
 import getpass
-token = input("Paste discord token: ")
-password = getpass.getpass("Enter password: ")
-fileobj = fileio(".bot_token", password)
-fileobj.write(token.encode("utf8"))
-fileobj.close()
+
+def gentoken():
+	token = input("Paste discord token: ")
+	fileobj = fileio(".bot_token", getpass.getpass("Enter password: "))
+	fileobj.write(token.encode("utf8"))
+	fileobj.close()
+
+if __name__ == "__main__":
+	gentoken()
