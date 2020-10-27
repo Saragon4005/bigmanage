d05619c910de52fcc8f6e216d04c78e53fae10ee
ultrabear <bearodark@gmail.com>
2020-10-26 17:15:44 -0700

    version bump oops

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from discord.ext import commands
 
 from SKCY11X import fileio as SKCYfileio
 
-print("BIGMANAGE BOT B20201026.0")  # X means borked, B is beta, R is release
+print("BIGMANAGE BOT B20201026.1")  # X means borked, B is beta, R is release
 
 logger.start()
 
